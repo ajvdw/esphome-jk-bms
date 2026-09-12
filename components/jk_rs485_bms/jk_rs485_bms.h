d1d10b48760fc4281fc26abe239bd2c621e13cd4
Arie Johan van de Werken <ajvandewerken@gmail.com>
2026-09-12 14:03:56 +0200

    Update include paths for jk_switch and jk_number

--- components/jk_rs485_bms/jk_rs485_bms.h
+++ components/jk_rs485_bms/jk_rs485_bms.h
@@ -1,7 +1,7 @@
 #pragma once
 
-// #include "../jkrs485_bms/switch/jk_switch.h"
-// #include "../jkrs485_bms/number/jk_number.h"
+#include "number/jk_number.h"
+#include "switch/jk_switch.h"
 #include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
